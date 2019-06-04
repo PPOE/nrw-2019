f328c852c132d35f0cf34aaf065a6d88f5b775e9
Julian Seidl <julian@julianseidl.at>
2019-06-04 12:21:51 +0200

    Added Index

--- front-page.php
+++ front-page.php
@@ -10,7 +10,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                
+                    <?php if (have_posts()): ?>
+                        <?php while (have_posts()): the_post(); ?>
+
+                            <?php the_content(); ?>
+
+                        <?php endwhile; ?>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php get_header(); ?>
 
 <main class="content">
+    <div class="page-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <?php if (have_posts()): ?>
+                        <?php while (have_posts()): the_post(); ?>
 
-    <?php if (have_posts()): ?>
-        <?php while (have_posts()): the_post(); ?>
+                            <?php the_content(); ?>
 
-            <?php the_content(); ?>
-
-        <?php endwhile; ?>
-    <?php endif; ?>
-
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
         
 <?php get_footer(); ?>
