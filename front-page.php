215dce9cd605e91618203f74d69f9bc9694c3173
Julian Seidl <julian@julianseidl.at>
2019-06-04 11:50:21 +0200

    CSS
    Removed Unused CSS folder
    Added Fonts

--- front-page.php
+++ front-page.php
@@ -6,9 +6,12 @@
 ?>
 <main class="content">
     <div class="header-image" style="background-image: url('<?php echo $image ?>')"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
+    <div class="page-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                
+                </div>
             </div>
         </div>
     </div>
