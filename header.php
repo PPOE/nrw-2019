21cb434e8c379f635f0c77029493baa5e710ae95
Julian Seidl <julian@julianseidl.at>
2019-06-04 11:10:14 +0200

    Header Update

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+        <title><?php wp_title(); ?></title>
 
         <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_url'); ?>/img/favicon/apple-icon-57x57.png">
         <link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('template_url'); ?>/apple-icon-60x60.png">
