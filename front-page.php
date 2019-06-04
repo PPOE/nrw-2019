bb29429dcaaa1f4faff0c4fcafb30d14d1bf5585
Julian Seidl <julian@julianseidl.at>
2019-06-04 11:37:46 +0200

    Added Header-Image Support

--- front-page.php
+++ front-page.php
@@ -1,7 +1,11 @@
 <?php get_header(); ?>
 
+<?php 
+    $imageSource = get_field('header-image');
+    $image = $imageSource['sizes']['header-image'];
+?>
 <main class="content">
-    <div class="header-image" style="background-image: url('<?php  ?>')"></div>
+    <div class="header-image" style="background-image: url('<?php echo $image ?>')"></div>
     <div class="container">
         <div class="row">
             <div class="col-12">
--- functions/theme/theme-image-sizes.php
+++ functions/theme/theme-image-sizes.php
@@ -8,5 +8,6 @@
 function ppat_image_sizes() {
     add_image_size('boilerplate-slider', 1920, 1080);
     add_image_size('boilerplate-thumbnail', 300, 300);
+    add_image_size('header-image', 1080, 9999);
 }
 add_action('after_setup_theme', 'ppat_image_sizes');
