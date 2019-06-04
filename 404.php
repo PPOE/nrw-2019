aff856aa834eec9bbb7a514f3d9c768a1de51312
Julian Seidl <julian@julianseidl.at>
2019-06-04 15:26:11 +0200

    added 404 page

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<main class="content">
+    <div class="page-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="page-not-found">
+                        <h1>404 - Seite nicht gefunden</h1>
+                        <p>Die gewünschte Seite wurde leider nicht gefunden!</p>
+                        <img src="https://media.giphy.com/media/9J7tdYltWyXIY/giphy.gif" alt="404 Seite nicht gefunden!">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
