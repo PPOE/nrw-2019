728ecd8ffea4ecb7db2ac364e027c2a463804e40
Julian Seidl <julian@julianseidl.at>
2019-06-04 11:33:54 +0200

    Started Frontpage

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<main class="content">
+    <div class="header-image" style="background-image: url('<?php  ?>')"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
