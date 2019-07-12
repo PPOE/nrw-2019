26f33813b10093c221af32e1b012817e94841959
Julian Seidl <julian@julianseidl.at>
2019-07-12 10:51:31 +0200

    Header new

--- front-page.php
+++ front-page.php
@@ -6,6 +6,56 @@
 ?>
 <main class="content">
     <div class="header-image" style="background-image: url('<?php echo $image ?>')"></div>
+    <div class="link-boxes">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg">
+                    <div class="link-box">
+                        <a href="#">
+                            <div class="display-table">
+                                <div class="display-table-cell">
+                                    <span>Spenden</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg">
+                    <div class="link-box">
+                        <a href="#">
+                            <div class="display-table">
+                                <div class="display-table-cell">
+                                    <span>Transparenz</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg">
+                    <div class="link-box">
+                        <a href="#">
+                            <div class="display-table">
+                                <div class="display-table-cell">
+                                    <span>Spendenverlauf</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg">
+                    <div class="link-box">
+                        <a href="#">
+                            <div class="display-table">
+                                <div class="display-table-cell">
+                                    <span>Ich bin ein Link</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="page-content">
         <div class="container">
             <div class="row">
