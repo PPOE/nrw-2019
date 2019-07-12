e9f47da1c853472d4dadd50f1ddc3a58b47f9b7e
Julian Seidl <julian@julianseidl.at>
2019-07-12 11:31:51 +0200

    Started w/index

--- index.php
+++ index.php
@@ -1,6 +1,38 @@
 <?php get_header(); ?>
 
+<?php 
+    $imageSource = get_field('header-image');
+    $image = $imageSource['sizes']['header-image'];
+
+?>
 <main class="content">
+    <div class="header-image" style="background-image: url('<?php echo $image ?>')"></div>
+    <div class="link-boxes">
+        <div class="container">
+            <div class="row">
+                <?php 
+                    $post_objects = get_field('link_blocks', 'options');
+                    if( $post_objects ): 
+                ?>
+                    <?php foreach( $post_objects as $post):?>
+                        <?php setup_postdata($post); ?>
+                            <div class="col-lg">
+                                <div class="link-box">
+                                    <a href="<?php the_permalink();?>">
+                                        <div class="display-table">
+                                            <div class="display-table-cell">
+                                                <span><?php the_title(); ?></span>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                    <?php endforeach; ?>
+                    <?php wp_reset_postdata(); ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
     <div class="page-content">
         <div class="container">
             <div class="row">
@@ -17,5 +49,5 @@
         </div>
     </div>
 </main>
-        
+
 <?php get_footer(); ?>
